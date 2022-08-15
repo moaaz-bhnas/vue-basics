bd0f687f73b07f91b8561d287d0a1926833d9c48
moaaz-bhnas <moaaz_bs@yahoo.com>
2022-08-15 19:16:38 +0200

    outpu data and react to click events

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const app = Vue.createApp();
+/*
+  - This object represents the "root component"
+  - It's the component that will be controlling the app section 
+  - This object contains
+    * data
+    * functions (e.g. to react to events)
+    * component template
+*/
+const app = Vue.createApp({
+  data() {
+    return {
+      number: 0,
+    };
+  },
+});
 
 app.mount("#app");
